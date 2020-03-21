e73be1a5fef7a15737ff935f6e599e5270dd0df2
maldinuribrahim <maldinuribrahim@gmail.com>
2020-03-21 18:06:04 +0700

    Optiomize API Auth

--- Routes/web.php
+++ Routes/web.php
@@ -11,46 +11,50 @@
 |
 */
 
-Route::group(['prefix' => 'control', 'middleware' => 'core.menu'], function() {
+Route::group(['prefix' => 'control', 'middleware' => 'core.auth'], function() {
     
-	Route::group(['middleware' => 'core.auth'], function() {
+	Route::group(['prefix' => 'taxonomy'], function() {
 
-		Route::group(['prefix' => 'taxonomy'], function() {
-	        /*=============================================
-	        =            Taxonomy CMS            =
-	        =============================================*/
-	        
+        /*=============================================
+        =            Taxonomy CMS            =
+        =============================================*/
+
+        Route::group(['middleware' => 'core.menu'], function() {
 	        Route::get('master', 'TaxonomyController@index')->name('taxonomy')->middleware('can:menu-taxonomy');
-			    Route::get('form', 'TaxonomyController@create')->name('taxonomy');
-			    Route::post('form', 'TaxonomyController@store')->name('taxonomy')->middleware('can:create-taxonomy');
-			    Route::put('form', 'TaxonomyController@store')->name('taxonomy');
-			    Route::delete('form', 'TaxonomyController@destroy')->name('taxonomy');
+		    Route::get('form', 'TaxonomyController@create')->name('taxonomy');
+		    Route::post('form', 'TaxonomyController@store')->name('taxonomy')->middleware('can:create-taxonomy');
+		    Route::put('form', 'TaxonomyController@store')->name('taxonomy');
+		    Route::delete('form', 'TaxonomyController@destroy')->name('taxonomy');
+        });
 
-			    Route::group(['prefix' => 'api'], function() {
-				    Route::get('master', 'TaxonomyController@serviceMaster');
-				    Route::get('suggestion-tag', 'TaxonomyController@getSuggestionTag');
-			    });
-	        
-	        /*=====  End of Taxonomy CMS  ======*/
-		});
+	    Route::group(['prefix' => 'api'], function() {
+		    Route::get('master', 'TaxonomyController@serviceMaster');
+		    Route::get('suggestion-tag', 'TaxonomyController@getSuggestionTag');
+	    });
+        
+        /*=====  End of Taxonomy CMS  ======*/
+        
+	});
 
-		Route::group(['prefix' => 'terms'], function() {
-	        /*=============================================
-	        =            Taxonomy CMS            =
-	        =============================================*/
-	        
+	Route::group(['prefix' => 'terms'], function() {
+
+        /*=============================================
+        =            Taxonomy CMS            =
+        =============================================*/
+        
+			Route::group(['middleware' => 'core.menu'], function() {
 			    Route::get('master', 'TermsController@index')->name('terms')->middleware('can:menu-taxonomy');
 			    Route::get('form', 'TermsController@create')->name('terms');
 			    Route::post('form', 'TermsController@store')->name('terms')->middleware('can:create-taxonomy');
 			    Route::put('form', 'TermsController@store')->name('terms');
 			    Route::delete('form', 'TermsController@destroy')->name('terms');
+			});
 
-			    Route::group(['prefix' => 'api'], function() {
-				    Route::get('master', 'TermsController@serviceMaster');
-			    });
-	        
-	        /*=====  End of Taxonomy CMS  ======*/
-		});
+		    Route::group(['prefix' => 'api'], function() {
+			    Route::get('master', 'TermsController@serviceMaster');
+		    });
         
+        /*=====  End of Taxonomy CMS  ======*/
 	});
+	
 });
